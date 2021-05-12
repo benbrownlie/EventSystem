4078df29a61768f074ef99afa37d9de097244cf4
Benjamin Brownlie <benjibspam@gmail.com>
2021-05-12 14:17:10 -0500

    Initial commit

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour
+{
+    /// <summary>
+    /// Turns the game object on/off
+    /// </summary>
+    public void ToggleCube()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
